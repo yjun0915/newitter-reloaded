fdb7045596700052cc5f90f6158ce7dbe4c784c0
yjun0915 <kyjun09150916@gmail.com>
2024-02-23 15:12:01 +0900

    feat: make router

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,25 @@
+import { createBrowserRouter } from "react-router-dom"
+import Layout from "./components/layout"
+import Home from "./routs/home"
+import Profile from "./routs/profile"
+
+const router = createBrowserRouter([
+  {
+    path: "/", 
+    element: <Layout/>, 
+    children: [
+      {
+        path: "", 
+        element: <Home/>, 
+      }, 
+      {
+        path: "profile", 
+        element: <Profile/>, 
+      }
+    ]
+  }
+])
+
 function App() {
 
   return (
