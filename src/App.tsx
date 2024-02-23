50b2df08d144aa9e4a86f22d0a4e12d672fde5ca
yjun0915 <kyjun09150916@gmail.com>
2024-02-23 15:37:38 +0900

    feat: component Create Account

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Layout from "./components/layout"
 import Home from "./routs/home"
 import Profile from "./routs/profile"
 import Login from "./routs/login"
+import CreateAccount from "./routs/create-account"
 
 const router = createBrowserRouter([
   {
@@ -14,12 +15,16 @@ const router = createBrowserRouter([
         element: <Home/>, 
       }, 
       {
-        path: "profile", 
+        path: "/profile", 
         element: <Profile/>, 
       }, 
       {
-        path: "login", 
+        path: "/login", 
         element: <Login/>,  
+      }, 
+      {
+        path: "/create-account", 
+        element: <CreateAccount/>, 
       }
     ]
   }
