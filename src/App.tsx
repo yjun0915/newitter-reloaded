6b05188d69312e730750d156c512c74e59a3124d
yjun0915 <kyjun09150916@gmail.com>
2024-02-23 15:52:41 +0900

    style: set global style

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import Home from "./routs/home"
 import Profile from "./routs/profile"
 import Login from "./routs/login"
 import CreateAccount from "./routs/create-account"
+import { createGlobalStyle } from "styled-components"
+import reset from "styled-reset"
 
 const router = createBrowserRouter([
   {
@@ -30,10 +32,25 @@ const router = createBrowserRouter([
   }
 ])
 
+const GlobalStyles = createGlobalStyle`
+  ${reset};
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    background-color: black;
+    color: white;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue',
+    sans-self;
+  }
+`;
+
 function App() {
 
   return (
     <>
+      <GlobalStyles />
       <RouterProvider router={router} />
     </>
   )
