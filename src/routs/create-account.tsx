8d590d3ced9ceedbf0200aa77b5a1ccaa4496989
yjun0915 <kyjun09150916@gmail.com>
2024-02-24 15:34:33 +0900

    feat: create basic inputs

--- src/routs/create-account.tsx
+++ src/routs/create-account.tsx
@@ -1,3 +1,25 @@
-export default function CreateAccount(){
-    return <h1>Create Account</h1>
+import styled from "styled-components";
+
+const Wrapper = styled.div``;
+
+const Form = styled.form``;
+
+const Input = styled.input``;
+
+export default function CreateAccount() {
+  return (
+    <Wrapper>
+      <Form>
+        <Input name="name" placeholder="Name" type="text" />
+        <Input name="email" placeholder="Email" type="email" />
+        <Input
+          name="password"
+          placeholder="Password"
+          type="password"
+          required
+        />
+        <Input type="submit" value="Create Account" />
+      </Form>
+    </Wrapper>
+  );
 }
