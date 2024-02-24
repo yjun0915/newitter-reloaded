0ce95ec19ca32796307f3b31af745e0c3dde8234
yjun0915 <kyjun09150916@gmail.com>
2024-02-24 15:48:39 +0900

    feat: create onChange function

--- src/routs/create-account.tsx
+++ src/routs/create-account.tsx
@@ -12,13 +12,38 @@ export default function CreateAccount() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { name: value },
+    } = e;
+    if (name === "name") {
+      setName(value);
+    } else if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
 
   return (
     <Wrapper>
       <Form>
-        <Input name="name" value={name} placeholder="Name" type="text" />
-        <Input name="email" value={email} placeholder="Email" type="email" />
         <Input
+          onCahnge={onChange}
+          name="name"
+          value={name}
+          placeholder="Name"
+          type="text"
+        />
+        <Input
+          onCahnge={onChange}
+          name="email"
+          value={email}
+          placeholder="Email"
+          type="email"
+        />
+        <Input
+          onCahnge={onChange}
           name="password"
           value={password}
           placeholder="Password"
