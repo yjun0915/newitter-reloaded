803becf1b2a37009b54ffa87c8c652cbbf43e1cb
yjun0915 <kyjun09150916@gmail.com>
2024-02-24 15:41:31 +0900

    feat: create name, email, password state

--- src/routs/create-account.tsx
+++ src/routs/create-account.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 const Wrapper = styled.div``;
@@ -7,13 +8,19 @@ const Form = styled.form``;
 const Input = styled.input``;
 
 export default function CreateAccount() {
+  const [isLoading, setLoading] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <Wrapper>
       <Form>
-        <Input name="name" placeholder="Name" type="text" />
-        <Input name="email" placeholder="Email" type="email" />
+        <Input name="name" value={name} placeholder="Name" type="text" />
+        <Input name="email" value={email} placeholder="Email" type="email" />
         <Input
           name="password"
+          value={password}
           placeholder="Password"
           type="password"
           required
