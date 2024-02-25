244c7b8ea022748e4b0206a2600966a4d0b834b7
yjun0915 <kyjun09150916@gmail.com>
2024-02-25 17:49:08 +0900

    style: set button style

--- src/components/github-btn.tsx
+++ src/components/github-btn.tsx
@@ -1,9 +1,19 @@
 import styled from "styled-components"
 
 const Button = styled.span`
-  margin-top: 20px;
+  margin-top: 50px;
+  width: 100%;
   background-color: white;
   color: black;
+  font-weight: 500;
+  padding: 10px 20px;
+  border-radius: 50px;
+  border: 0;
+  display: flex;
+  gap: 5px;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
 `
 
 const Logo = styled.img`
