a127c4a261f6e7ab814d3c52502d430fda56c2e6
yjun0915 <kyjun09150916@gmail.com>
2024-02-25 16:49:48 +0900

    feat: make up log in

--- src/routs/login.tsx
+++ src/routs/login.tsx
@@ -1,3 +1,106 @@
-export default function Login(){
-    return <h1>Login</h1>
+import { useState } from "react"
+import styled from "styled-components"
+import { useNavigate } from "react-router-dom"
+import { FirebaseError } from "firebase/app"
+import { signInWithEmailAndPassword } from "firebase/auth"
+import { auth } from "../firebase"
+
+const Wrapper = styled.div`
+  hheight: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 420px;
+  padding: 50px 0px;
+`
+
+const Title = styled.h1`
+  font-size: 42px;
+`
+
+const Form = styled.form`
+  margin-top: 50px;
+  margin-bottom: 10px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  width: 100%;
+`
+
+const Input = styled.input`
+  padding: 10px 20px;
+  border-radius: 50px;
+  border: none;
+  width: 100%;
+  font-size: 16px;
+  &[type="submit"] {
+    cursor: pointer;
+    &: hover {
+      opacity: 0.8;
+    }
+  }
+`
+
+const Error = styled.span`
+  font-weight: 600;
+  color: tomato;
+`
+
+export default function Login() {
+  const navigate = useNavigate()
+  const [isLoading, setLoading] = useState(false)
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [error, setError] = useState("")
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { name, value },
+    } = e
+    if (name === "email") {
+      setEmail(value)
+    } else if (name === "password") {
+      setPassword(value)
+    }
+  }
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    setError("")
+    if (isLoading || email === "" || password === "") return
+    try {
+      setLoading(true)
+      await signInWithEmailAndPassword(auth, email, password)
+      navigate("/")
+    } catch (e) {
+      if (e instanceof FirebaseError) {
+        setError(e.message)
+      }
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  return (
+    <Wrapper>
+      <Title>Login to 𝕏</Title>
+      <Form onSubmit={onSubmit}>
+        <Input
+          onChange={onChange}
+          name="email"
+          value={email}
+          placeholder="Email"
+          type="email"
+        />
+        <Input
+          onChange={onChange}
+          name="password"
+          value={password}
+          placeholder="Password"
+          type="password"
+          required
+        />
+        <Input type="submit" value={isLoading ? "Loading..." : "Login"} />
+      </Form>
+      {error !== "" ? <Error>{error}</Error> : null}
+    </Wrapper>
+  )
 }
