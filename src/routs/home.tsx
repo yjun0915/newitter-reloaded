7c62af657668317866942260b9998bce4fb84c86
yjun0915 <kyjun09150916@gmail.com>
2024-02-25 15:24:48 +0900

    feat: create log out button

--- src/routs/home.tsx
+++ src/routs/home.tsx
@@ -1,3 +1,13 @@
-export default function Home(){
-    return <h1>Home</h1>
+import { auth } from "../firebase"
+
+export default function Home() {
+  const logOut = () => {
+    auth.signOut()
+  }
+
+  return (
+    <h1>
+      <button onClick={logOut}>Log Out</button>
+    </h1>
+  )
 }
