03cd5941c46927fdaa3bcc3fcb6feeb85f1b9a2a
yjun0915 <kyjun09150916@gmail.com>
2024-02-28 15:36:25 +0900

    feat: function onSubmit

--- src/components/post-tweet-form.tsx
+++ src/components/post-tweet-form.tsx
@@ -1,5 +1,7 @@
+import { addDoc, collection } from "firebase/firestore"
 import { useState } from "react"
 import styled from "styled-components"
+import { auth, db } from "../firebase"
 
 const Form = styled.form`
   display: flex;
@@ -79,9 +81,23 @@ export default function PostTweetForm() {
       setFile(files[0])
     }
   }
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-    if (isLoading || tweet === "" || tweet.length > 180) return
+    const user = auth.currentUser
+    if (!user || isLoading || tweet === "" || tweet.length > 180) return
+    try {
+      setLoading(true)
+      await addDoc(collection(db, "tweets"), {
+        tweet,
+        createdAt: Date.now(),
+        username: user.displayName || "Anonymous",
+        userID: user.uid,
+      })
+    } catch (e) {
+      console.log(e)
+    } finally {
+      setLoading(false)
+    }
   }
   return (
     <Form onSubmit={onSubmit}>
