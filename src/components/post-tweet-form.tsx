a2effc8cc0151e4e3b22fcdb7129568ff2025220
yjun0915 <kyjun09150916@gmail.com>
2024-02-28 14:20:50 +0900

    style: set TextArea style

--- src/components/post-tweet-form.tsx
+++ src/components/post-tweet-form.tsx
@@ -1,8 +1,40 @@
 import styled from "styled-components"
 
-const Form = styled.form``
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`
 
-const TextArea = styled.textarea``
+const TextArea = styled.textarea`
+  border: 2px solid white;
+  padding: 20px;
+  border-radius: 20px;
+  font-size: 16px;
+  color: white;
+  background-color: black;
+  width: 100%;
+  resize: none;
+  &::placeholder {
+    font-size: 16px;
+    font-family:
+      system-ui,
+      -apple-system,
+      BlinkMacSystemFont,
+      "Segoe UI",
+      Roboto,
+      Oxygen,
+      Ubuntu,
+      Cantarell,
+      "Open Sans",
+      "Helvetica Neue",
+      sans-serif;
+  }
+  &:focus {
+    outline: none;
+    border-color: #1d6bf0;
+  }
+`
 
 const AttachFileButton = styled.label``
 
