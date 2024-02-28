3b16a2607466f4d1fa842823dd317b9090491a7f
yjun0915 <kyjun09150916@gmail.com>
2024-02-28 17:02:18 +0900

    feat: fetching tweets basic construction

--- src/components/timeline.tsx
+++ src/components/timeline.tsx
@@ -1,3 +1,21 @@
+import { useEffect, useState } from "react"
+import styled from "styled-components"
+
+export interface ITweet {
+  photo: string
+  tweet: string
+  userId: string
+  username: string
+  updatedAt: number
+}
+
+const Wrapper = styled.div``
+
 export default function TimeLine() {
-  return <></>
+  const [tweets, setTweets] = useState<ITweet[]>([])
+  const fetchTweets = async () => {}
+  useEffect(() => {
+    fetchTweets()
+  }, [])
+  return <Wrapper>{JSON.stringify(tweets)}</Wrapper>
 }
