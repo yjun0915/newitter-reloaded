90cbaac998d3e012bd6e16e14d0b848bc64a65a6
yjun0915 <kyjun09150916@gmail.com>
2024-02-28 17:06:21 +0900

    feat: get tweets using query

--- src/components/timeline.tsx
+++ src/components/timeline.tsx
@@ -1,5 +1,7 @@
+import { collection, orderBy, query } from "firebase/firestore"
 import { useEffect, useState } from "react"
 import styled from "styled-components"
+import { db } from "../firebase"
 
 export interface ITweet {
   photo: string
@@ -13,7 +15,12 @@ const Wrapper = styled.div``
 
 export default function TimeLine() {
   const [tweets, setTweets] = useState<ITweet[]>([])
-  const fetchTweets = async () => {}
+  const fetchTweets = async () => {
+    const tweetsQuery = query(
+      collection(db, "tweets"),
+      orderBy("createdAt", "desc"),
+    )
+  }
   useEffect(() => {
     fetchTweets()
   }, [])
