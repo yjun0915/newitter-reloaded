d45936e3eb1ed22af3e6c4487ccdb1fb199b1e4f
yjun0915 <kyjun09150916@gmail.com>
2024-02-29 14:20:36 +0900

    style: create dlelteButton

--- src/components/tweet.tsx
+++ src/components/tweet.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components"
 import { ITweet } from "./timeline"
+import { auth } from "../firebase"
 
 const Wrapper = styled.div`
   display: grid;
@@ -27,12 +28,27 @@ const Photo = styled.img`
   border-radius: 15px;
 `
 
-export default function Tweet({ username, photo, tweet }: ITweet) {
+const DeleteButton = styled.button`
+  background-color: tomato;
+  color: white;
+  font-weight: 600;
+  border: 0;
+  font-size: 12px;
+  padding: 5px 10px;
+  text-transform: uppercase;
+  border-radius: 5px;
+  cursor: pointer;
+`
+
+export default function Tweet({ username, photo, tweet, userId }: ITweet) {
+  const user = auth.currentUser
+
   return (
     <Wrapper>
       <Column>
         <Username>{username}</Username>
         <Payload>{tweet}</Payload>
+        {user?.uid === userId ? <DeleteButton>Delete</DeleteButton> : null}
       </Column>
       <Column>{photo ? <Photo src={photo} /> : null}</Column>
     </Wrapper>
