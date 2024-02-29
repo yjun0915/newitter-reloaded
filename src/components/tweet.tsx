889b5a0711c0fa976ec00097971e10259d14775f
yjun0915 <kyjun09150916@gmail.com>
2024-02-29 17:54:53 +0900

    feat: connect to edit tweet form

--- src/components/tweet.tsx
+++ src/components/tweet.tsx
@@ -3,6 +3,8 @@ import { ITweet } from "./timeline"
 import { auth, db, storage } from "../firebase"
 import { deleteDoc, doc } from "firebase/firestore"
 import { deleteObject, ref } from "firebase/storage"
+import { useState } from "react"
+import EditTweetForm from "./edit-tweet-form"
 
 const Wrapper = styled.div`
   display: grid;
@@ -61,6 +63,7 @@ const EditButton = styled.button`
 
 export default function Tweet({ username, photo, tweet, userId, id }: ITweet) {
   const user = auth.currentUser
+  const [isEditing, setEditing] = useState(false)
   const onDelete = async () => {
     const ok = confirm("Are you sure you want to delete this tweet?")
     if (!ok || user?.uid !== userId) return
@@ -75,16 +78,21 @@ export default function Tweet({ username, photo, tweet, userId, id }: ITweet) {
     } finally {
     }
   }
+  const onEdit = () => {
+    setEditing(true)
+  }
   return (
     <Wrapper>
       <Column>
         <Username>{username}</Username>
-        <Payload>{tweet}</Payload>
+        {isEditing ? <EditTweetForm /> : <Payload>{tweet}</Payload>}
         <Buttons>
           {user?.uid === userId ? (
-            <DeleteButton onClick={onDelete}>Delete</DeleteButton>
+            <DeleteButton onClick={onDelete}>DELETE</DeleteButton>
           ) : null}
-          {user?.uid === userId ? <EditButton>Edit</EditButton> : null}
+          {user?.uid === userId ? (
+            <EditButton onClick={onEdit}>EDIT</EditButton>
+          ) : null}
         </Buttons>
       </Column>
       <Column>{photo ? <Photo src={photo} /> : null}</Column>
