d22ecd295c9d9d1fd803c785505fa4e69498b5e6
yjun0915 <kyjun09150916@gmail.com>
2024-03-01 18:54:17 +0900

    style: create profile div

--- src/routs/profile.tsx
+++ src/routs/profile.tsx
@@ -1,3 +1,27 @@
-export default function Profile(){
-    return <h1>Profile</h1>
+import styled from "styled-components"
+import { auth } from "../firebase"
+
+const Wrapper = styled.div``
+
+const AvatarUpload = styled.label``
+
+const AvatarImage = styled.img``
+
+const AvatarInput = styled.input`
+  display: none;
+`
+
+const Name = styled.span``
+
+export default function Profile() {
+  const user = auth.currentUser
+  return (
+    <Wrapper>
+      <AvatarUpload>
+        <AvatarImage />
+      </AvatarUpload>
+      <AvatarInput type="file" accept="image/*" />
+      <Name>{user?.displayName ?? "Anonimous"}</Name>
+    </Wrapper>
+  )
 }
